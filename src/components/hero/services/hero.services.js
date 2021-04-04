927e11e3086bfd328e70d7f1c9f68704e557b893
Wilson Andres Naranjo Romero <wanaranjo@gmail.com>
2021-04-04 13:09:30 -0500

    create Hero services

---
+++ src/components/hero/services/hero.services.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const API_URL = process.env.API_URL
+
+export const createHeaders = () => {
+  const headers = {
+    'accept': 'application/json',
+    'Content-Type': 'application/json'
+  }
+  return headers;
+}
+
+export const getHeroes = async () => {
+  try {
+    const headers = createHeaders();
+    // headers['Authorization'] = `Bearer ${token}`;
+    const url = `${API_URL}/heroes`;
+    const { data } = await axios.get(url, { headers });
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error('getHeros:', error);
+    return error;
+  }
+}
