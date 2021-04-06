367c1d3aee05728bd00b1ecb3874135d303c5c2e
Wilson Andres Naranjo Romero <wanaranjo@gmail.com>
2021-04-06 08:47:32 -0500

    [update] components

--- src/components/shared/ui/MainHome.svelte
+++ src/components/shared/ui/MainHome.svelte
@@ -1,11 +1,11 @@
 <script>
-  import ListProducts from "../../products/ui/ListProducts.svelte";
+  import Hero from "../../hero/ui/Hero.svelte";
+  import DetailProduct from "../../product/ui/DetailProduct.svelte";
   import Category from "./Category.svelte";
-  import Hero from "./Hero.svelte";
 </script>
 
 <div class="flex flex-col">
   <Hero />
   <Category />
-  <ListProducts />
+  <DetailProduct />
 </div>
